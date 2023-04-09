84354791e9dd7fb052623b1ade2dd4018b341ba6
hideyoshidan <iceblue_0830@yahoo.co.jp>
2023-04-09 22:30:57 +0900

    modify around docker

---
+++ src/services/user/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "todo.com/proto/user"
+	rpc "todo.com/services/user/rpc"
+)
+
+const (
+	RPC_PORT = 4000
+)
+
+func main() {
+	log.Print("USer Server Starting")
+
+	listenProt, err := net.Listen("tcp", fmt.Sprintf(":%d", RPC_PORT))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpc := grpc.NewServer()
+	rpc := rpc.NewRPC()
+	pb.RegisterGreeterServer(grpc, rpc)
+	reflection.Register(grpc)
+
+	if err := grpc.Serve(listenProt); err != nil {
+		log.Fatal(err)
+	}
+}
